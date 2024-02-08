e173cde0310061fb965b8bd4b17371115308ac11
Oliulla <razasm29@gmail.com>
2024-02-08 20:21:58 +0600

    :tada: Initial Setup done

---
+++ README.md
@@ -0,0 +1,13 @@
+## Getting Started
+
+First, prepare the server
+
+```bash
+npm install
+```
+
+Then, run the development server:
+
+```bash
+npm run dev
+```
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Done It?</title>
+        <meta name="description" content="Todo Application" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Setup done</h1>
+      </main>
+    </>
+  );
+}
